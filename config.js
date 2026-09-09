81503700ac2907c38ea1cb0d11c65ef803ea328a
DaveTech11 <techdave221@gmail.com>
2026-09-09 12:30:50 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // Get this from @BotFather
-  BOT_TOKEN: '8806610904:AAFJ8UCyle_KyRodbkPT14jl8_KjDdH4jyQ',
+  BOT_TOKEN: '8940844349:AAEv5d2Nvpmv4TTmXb8bL5uwC-nzWBCL0dc',
 
   // Automatically set this image as the bot profile picture every time the bot deploys/starts.
   UPDATE_PIC_ON_DEPLOY: true,
